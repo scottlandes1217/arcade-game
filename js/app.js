4e181bbb536cbf8f71d55e554194ac77fe29afb1
Scott Landes <scottlandes@01317-MBP13R.local>
2017-05-17 19:26:11 -0700

    Added enemey var

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,7 +17,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    this.x += this.speed * dt;
+}; olympus eden
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
