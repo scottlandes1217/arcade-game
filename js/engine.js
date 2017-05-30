a32815e0b459ef9089477111bae5207fb229aadc
Scott Landes <scottlandes@01317-MBP13R.local>
2017-05-29 20:53:27 -0700

    fixed scrolling issue - changed canvas height

--- js/engine.js
+++ js/engine.js
@@ -26,7 +26,7 @@ var Engine = (function(global) {
         lastTime;
 
     canvas.width = 1010; //505
-    canvas.height = 606;
+    canvas.height = 565; //606
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
