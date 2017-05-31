e7ebf4dfb7af9afbe459525e38cfe98cd9beeae6
Scott Landes <scottlandes@01317-MBP13R.local>
2017-05-30 19:57:06 -0700

    separated detection variables

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Variables to remove Magic Numbers
 var CANVAS_WIDTH = 910;
 var CANVAS_TOP = 30;
 var CANVAS_HEIGHT = 140;
@@ -36,17 +37,19 @@ Enemy.prototype.update = function(dt) {
 
 // make enemies loop to left side of canvas after reaching canvas.width
 Enemy.prototype.updatePosition = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for all computers.
     this.x += this.speed * dt;
+
+   // make enemies loop to left side of canvas after reaching canvas.width
     if (this.x >= CANVAS_WIDTH) {
         this.x = 0;
     }
-    detectCollision(this);
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    displayScoreLevel(score, gameLevel);
 };
 
 // Now write your own player class
@@ -65,15 +68,23 @@ var Player = function(x, y, speed) {
 
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 Player.prototype.update = function() {
-    // function not needed right now
+
+    //detect collision on canvas border
+    borderCollision(this);
+
+    //detect collision on enemies
+    detectCollision(this);
+
+    //Level Up Collision
+    LevelUp(this);
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    displayScoreLevel(score, gameLevel);
 };
 
 
@@ -109,11 +120,11 @@ var displayScoreLevel = function(aScore, aLevel) {
     document.body.insertBefore(scoreLevelDiv, firstCanvasTag[0]);
 };
 
-var detectCollision = function(anEnemy) {
-    // check for collision between enemy and player
+// check for collision between enemy and player
 
-    if (
-        player.x < enemy.x + enemy.width &&
+var detectCollision = function(anEnemy) {
+
+    if (player.x < enemy.x + enemy.width &&
         player.x + player.width > enemy.x &&
         player.y < enemy.y + enemy.height &&
         player.height + player.y > enemy.y) {
@@ -121,12 +132,30 @@ var detectCollision = function(anEnemy) {
         console.log('collided');
         player.x = (CANVAS_WIDTH / 2);
         player.y = CANVAS_BOTTOM;
-        score -= 1;
     }
-    // check for player reaching top of canvas and winning the game
-    // if player wins, add 1 to the score and level
-    // pass score as an argument to the increaseDifficulty function
-   
+};
+
+// check if player runs into left, bottom, or right canvas walls
+// prevent player from moving beyond canvas wall boundaries
+
+var borderCollision = function(hitBorder) {
+
+    if (player.y > CANVAS_BOTTOM) {
+        player.y = CANVAS_BOTTOM;
+    }
+    if (player.x > CANVAS_RIGHT) {
+        player.x = CANVAS_RIGHT;
+    }
+    if (player.x < CANVAS_LEFT) {
+        player.x = CANVAS_LEFT;
+    }
+};
+
+// check for player reaching top of canvas and winning the game
+// if player wins, add 1 to the score and level
+
+var LevelUp = function(LevelUp) {   
+
     if (player.y + CANVAS_TOP <= 0) {
 
         player.x = (CANVAS_WIDTH / 2);
@@ -139,21 +168,11 @@ var detectCollision = function(anEnemy) {
         increaseDifficulty(score);
 
     }
-
-    // check if player runs into left, bottom, or right canvas walls
-    // prevent player from moving beyond canvas wall boundaries
-    if (player.y > CANVAS_BOTTOM) {
-        player.y = CANVAS_BOTTOM;
-    }
-    if (player.x > CANVAS_RIGHT) {
-        player.x = CANVAS_RIGHT;
-    }
-    if (player.x < CANVAS_LEFT) {
-        player.x = CANVAS_LEFT;
-    }
 };
 
+// Pass score as an argument to the increaseDifficulty function
 // Increase number of enemies on screen based on player's score
+// Place all enemy objects in an array called allEnemies
 var increaseDifficulty = function(numEnemies) {
     // remove all previous enemies on canvas
     allEnemies.length = 0;
